b7e2c940e86549c5eeb541afea2ea23b3e00f3b3
arthurbiehl <biehlarthurv@gmail.com>
2024-06-14 16:25:15 -0300

    initial commit

---
+++ code/index.js
@@ -0,0 +1,437 @@
+const items = [
+    {
+        id: 0,
+        nome: 'Shelly',
+        sobre: 'nada ainda',
+        img: '../static/shelly.png',
+    },
+    {
+        id: 1,
+        nome: 'Nita',
+        img: '../static/Nita.png',
+    },
+    {
+        id: 2,
+        nome: 'Colt',
+        img: '../static/Colt.png',
+    },
+    {
+        id: 3,
+        nome: 'Bull',
+        img: '../static/Bull.png',
+    },
+    {
+        id: 3,
+        nome: 'Jessie',
+        img: '../static/Jessie.png',
+    },
+    {
+        id: 3,
+        nome: 'Brock',
+        img: '../static/Brock.png',
+    },
+    {
+        id: 3,
+        nome: 'Dynamike',
+        img: '../static/Dynamike.png',
+    },
+    {
+        id: 3,
+        nome: 'Bo',
+        img: '../static/Bo.png',
+    },
+    {
+        id: 3,
+        nome: 'EL Primo',
+        img: '../static/ElPrimo.png',
+    },
+    {
+        id: 3,
+        nome: 'Barley',
+        img: '../static/Barley.png',
+    },
+    {
+        id: 3,
+        nome: 'Poco',
+        img: '../static/Poco.png',
+    },
+    {
+        id: 3,
+        nome: 'Rico',
+        img: '../static/Rico.png',
+    },
+    {
+        id: 3,
+        nome: 'Corvo',
+        img: '../static/Corvo.png',
+    },
+    {
+        id: 3,
+        nome: 'Mortis',
+        img: '../static/Mortis.png',
+    },
+    {
+        id: 3,
+        nome: 'Spike',
+        img: '../static/Spike.png',
+    },
+    {
+        id: 3,
+        nome: 'Darryl',
+        img: '../static/Darryl.png',
+    },
+    {
+        id: 3,
+        nome: 'Penny',
+        img: '../static/Penny.png',
+    },
+    {
+        id: 3,
+        nome: 'Piper',
+        img: '../static/Piper.png',
+    },
+    {
+        id: 3,
+        nome: 'Pam',
+        img: '../static/Frank.png',
+    },
+    {
+        id: 3,
+        nome: 'Tara',
+        img: '../static/Tara.png',
+    },
+    {
+        id: 3,
+        nome: 'Leon',
+        img: '../static/Leon.png',
+    },
+    {
+        id: 3,
+        nome: 'Eugénio',
+        img: '../static/Eugenio.png',
+    },
+    {
+        id: 3,
+        nome: 'Carl',
+        img: '../static/Carl.png',
+    },
+    {
+        id: 3,
+        nome: 'Rosa',
+        img: '../static/Rosa.png',
+    },
+    {
+        id: 3,
+        nome: 'Bibi',
+        img: '../static/Bibi.png',
+    },
+    {
+        id: 3,
+        nome: 'Tick',
+        img: '../static/Tick.png',
+    },
+    {
+        id: 3,
+        nome: '8-BIT',
+        img: '../static/8-BIT.png',
+    },
+    {
+        id: 3,
+        nome: 'Sandy',
+        img: '../static/Sandy.png',
+    },
+    {
+        id: 3,
+        nome: 'EMZ',
+        img: '../static/EMZ.png',
+    },
+    {
+        id: 3,
+        nome: 'Bea',
+        img: '../static/Bea.png',
+    },
+    {
+        id: 3,
+        nome: 'Max',
+        img: '../static/Max.png',
+    },
+    {
+        id: 3,
+        nome: 'Mister P',
+        img: '../static/MisterP.png',
+    },
+    {
+        id: 3,
+        nome: 'Jacky',
+        img: '../static/Jacky.png',
+    },    {
+        id: 3,
+        nome: 'Sprout',
+        img: '../static/Sprout.png',
+    },
+    {
+        id: 3,
+        nome: 'Gale',
+        img: '../static/Gale.png',
+    },
+    {
+        id: 3,
+        nome: 'Nani',
+        img: '../static/Nani.png',
+    },
+    {
+        id: 3,
+        nome: 'Wattson (surge)',
+        img: '../static/Wattson.png',
+    },
+    {
+        id: 3,
+        nome: 'Colette',
+        img: '../static/Colette.png',
+    },
+    {
+        id: 3,
+        nome: 'Amber',
+        img: '../static/Amber.png',
+    },
+    {
+        id: 3,
+        nome: 'Lou',
+        img: '../static/Lou.png',
+    },
+    {
+        id: 3,
+        nome: 'Byron',
+        img: '../static/Byron.png',
+    },
+    {
+        id: 3,
+        nome: 'Edgar',
+        img: '../static/Edgar.png',
+    },
+    {
+        id: 3,
+        nome: 'Coronel Ruffus',
+        img: '../static/Coronel.png',
+    },
+    {
+        id: 3,
+        nome: 'Stu',
+        img: '../static/Stu.png',
+    },
+    {
+        id: 3,
+        nome: 'Belle',
+        img: '../static/bellu.png',
+    },
+    {
+        id: 3,
+        nome: 'Squeak',
+        img: '../static/Squeak.png',
+    },
+    {
+        id: 3,
+        nome: 'Buzz',
+        img: '../static/Buzz.png',
+    },
+    {
+        id: 3,
+        nome: 'Griff',
+        img: '../static/Griff.png',
+    },
+    {
+        id: 3,
+        nome: 'Ash',
+        img: '../static/Ash.png',
+    },
+    {
+        id: 3,
+        nome: 'Meg',
+        img: '../static/Meg.png',
+    },
+    {
+        id: 3,
+        nome: 'Lola',
+        img: '../static/Lola.png',
+    },
+    {
+        id: 3,
+        nome: 'Grom',
+        img: '../static/Grom.png',
+    },
+    {
+        id: 3,
+        nome: 'Fang',
+        img: '../static/Fang.png',
+    },
+    {
+        id: 3,
+        nome: 'Eve',
+        img: '../static/Eve.png',
+    },
+    {
+        id: 3,
+        nome: 'Janet',
+        img: '../static/Janet.png',
+    },
+    {
+        id: 3,
+        nome: 'Bonnie',
+        img: '../static/Bonnie.png',
+    },
+    {
+        id: 3,
+        nome: 'Otis',
+        img: '../static/Otis.png',
+    },
+    {
+        id: 3,
+        nome: 'Sam',
+        img: '../static/Sam.png',
+    },
+    {
+        id: 3,
+        nome: 'Gus',
+        img: '../static/Gus.png',
+    },
+    {
+        id: 3,
+        nome: 'Buster',
+        img: '../static/Buster.png',
+    },
+    {
+        id: 3,
+        nome: 'Gray',
+        img: '../static/Gray.png',
+    },
+    {
+        id: 3,
+        nome: 'Chester',
+        img: '../static/Chester.png',
+    },
+    {
+        id: 3,
+        nome: 'Mandy',
+        img: '../static/Mandy.png',
+    },
+    {
+        id: 3,
+        nome: 'R-T',
+        img: '../static/R-t.png',
+    },
+    {
+        id: 3,
+        nome: 'Willow',
+        img: '../static/Willow.png',
+    },
+    {
+        id: 3,
+        nome: 'Maisie',
+        img: '../static/Maisie.png',
+    },
+    {
+        id: 3,
+        nome: 'Hank',
+        img: '../static/Hank.png',
+    },
+    {
+        id: 3,
+        nome: 'Cordelius',
+        img: '../static/Cordelius.png',
+    },
+    {
+        id: 3,
+        nome: 'Doug',
+        img: '../static/Doug.png',
+    },
+    {
+        id: 3,
+        nome: 'Pearl',
+        img: '../static/Pearl.png',
+    },
+    {
+        id: 3,
+        nome: 'Chuck',
+        img: '../static/Chuck.png',
+    },
+    {
+        id: 3,
+        nome: 'Charlie',
+        img: '../static/Charlie.png',
+    },
+    {
+        id: 3,
+        nome: 'Mico',
+        img: '../static/Mico.png',
+    },
+    {
+        id: 3,
+        nome: 'Larry & Lawrie',
+        img: '../static/larry&Lawrie.png',
+    },
+    {
+        id: 3,
+        nome: 'Kit',
+        img: '../static/Kit.png',
+    },
+    {
+        id: 3,
+        nome: 'Angelo',
+        img: '../static/Angelo.png',
+    },
+    {
+        id: 3,
+        nome: 'Melodie',
+        img: '../static/Melodie.png',
+    },
+    {
+        id: 3,
+        nome: 'Lily',
+        img: '../static/Lily.png',
+    },
+    {
+        id: 3,
+        nome: 'Draco',
+        img: '../static/Draco.png',
+    },
+];
+
+function inicializarBonecos() {
+    var container_bonecos = document.getElementById('browles');
+    container_bonecos.innerHTML = '';
+
+    items.forEach(val => {
+        container_bonecos.innerHTML += `
+            <div class='bonecos-single'>
+                <img src="${val.img}" />
+                <h2>${val.nome}</h2>
+                <p>${val.sobre}</p>
+
+            </div>
+        `;
+    });
+}
+
+function buscarItensPorNome() {
+    const inputBusca = document.getElementById('inputBusca');
+    const termoBusca = inputBusca.value.trim().toLowerCase();
+
+    const itemsFiltrados = items.filter(item => item.nome.toLowerCase().includes(termoBusca));
+
+    var container_bonecos = document.getElementById('browles');
+    container_bonecos.innerHTML = ''; // Limpa o conteúdo anterior
+
+    itemsFiltrados.forEach(val => {
+        container_bonecos.innerHTML += `
+            <div class='bonecos-single'>
+                <img src="${val.img}" />
+                <h2>${val.nome}</h2>
+                <p>${val.sobre}</p>
+            </div>
+        `;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    inicializarBonecos();
+});
